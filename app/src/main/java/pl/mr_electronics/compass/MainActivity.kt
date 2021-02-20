23cdef4ea022775c576554e7979629040e4a819b
Seweryn <pikulikseweryn@gmail.com>
2021-02-20 01:04:05 +0100

    Obliczanie odległości z gps.

--- app/src/main/java/pl/mr_electronics/compass/MainActivity.kt
+++ app/src/main/java/pl/mr_electronics/compass/MainActivity.kt
@@ -1,14 +1,30 @@
 package pl.mr_electronics.compass
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.*
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener, LocationListener {
 
     var arrow1 = listOf(PointF(-35.0f, 0.0f), PointF(0.0f, -300.0f), PointF(35.0f, 0.0f))
     var arrow2 = listOf(PointF(-25.0f, 0.0f), PointF(0.0f, -220.0f), PointF(25.0f, 0.0f))
@@ -17,12 +33,71 @@ class MainActivity : AppCompatActivity() {
     var arrow5 = listOf(PointF(-25.0f, 0.0f), PointF(0.0f, 46.0f), PointF(25.0f, 0.0f))
     var arrow6 = listOf(PointF(-25.0f, 0.0f), PointF(0.0f, -46.0f), PointF(25.0f, 0.0f))
 
+    private val locationPermissionCode = 2
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var magneticField: Sensor
+    private lateinit var locationManager: LocationManager
+    var handler: Handler = Handler(Looper.getMainLooper())
+    var location_curr: Location? = null
+    var location_dest: Location? = null
+
+    var mGravity: FloatArray? = null
+    var mGeomagnetic: FloatArray? = null
+    var azimut: Float = 0f
+    var azimutAverage: Float = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawCompass(20f, 20f)
+        location_dest = Location(LocationManager.GPS_PROVIDER)
+        location_dest!!.latitude = 37.4723
+        location_dest!!.longitude = -122.221
+
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+            this.accelerometer = it
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.let {
+            this.magneticField = it
+        }
+
+        getLocation()
+
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                azimutAverage = azimutAverage * 0.8f + azimut * 0.2f
+
+                drawCompass(azimut, 20f)
+
+                if (location_dest != null && location_curr != null) {
+                    distanceInfo.text = String.format("Distance from the destination: %.0f m.", location_curr!!.distanceTo(location_dest));
+                } else {
+                    distanceInfo.text = "Enter the destination point.";
+                }
+
+                handler.postDelayed(this, 500)
+            }
+        },200)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, this.accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, this.magneticField, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this);
     }
 
     fun drawCompass(dirWalk: Float, dirDestination: Float) {
@@ -135,5 +210,60 @@ class MainActivity : AppCompatActivity() {
         canvas.drawPath(path, paint)
 
         canvas.restore()
+    }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5f, this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.i("Sensor", "onAccuracyChanged() ");
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+
+        if (mGravity != null && mGeomagnetic != null) {
+            var R = FloatArray(9)
+            var I = FloatArray(9)
+
+            if (SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic)) {
+
+                // orientation contains azimut, pitch and roll
+                var orientation = FloatArray(3)
+                SensorManager.getOrientation(R, orientation);
+
+                azimut = Math.toDegrees(orientation[0].toDouble()).toFloat()
+                //Log.i("Sensor", "azimut: " + azimut);
+            }
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        Log.i("Sensor", "onLocationChanged() ");
+        this.location_curr = location;
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                getLocation()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
